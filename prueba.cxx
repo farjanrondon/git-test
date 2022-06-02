290a3009f8b20280d980d0f2cdfca565f8454115
farjanrondon <farjanr.18@gmail.com>
2022-06-01 20:53:20 -0400

    Add cambios de prueba.cxx y prueba.exe

--- prueba.cxx
+++ prueba.cxx
@@ -1,1 +1,14 @@
-/* Comentario */
+/* Programa que cuenta del 1 al 10 */
+
+#include<stdio.h>
+
+int main(){
+
+	int i = 0;
+	while(i <= 10){
+		fprintf(stdout, "%i\n", i);
+		i += 1;
+	}
+
+	return 0;
+}
